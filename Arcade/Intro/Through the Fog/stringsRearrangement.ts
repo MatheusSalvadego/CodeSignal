e48739666def78f4c256741e90aafa66f70dfc9d
Salvadego Matheus <matheus.salvadego@escola.pr.gov.br>
2024-02-25 12:30:40 -0300

    Strings Rearrangement

--- Arcade/Intro/Through the Fog/stringsRearrangement.ts
+++ Arcade/Intro/Through the Fog/stringsRearrangement.ts
@@ -1,23 +1,40 @@
-function stringRearrangement(inputArray: string[]): boolean {
-  const charFrequencyMap = getFrequencyCount(inputArray);
-  for (const count of Object.values(charFrequencyMap)) {
-    if (count > 1) {
+function stringsRearrangement(inputArray: string[]) {
+  let isRearrangementPossible: boolean = false;
+  let currentSolution: string[] = [];
+  tryRearranging(inputArray, isRearrangementPossible, currentSolution);
+  return isRearrangementPossible;
+}
+
+function isValidSolution(solution: string[]) {
+  for (let i = 0; i < solution.length - 1; i++) {
+    let diffCount = 0;
+    for (let j = 0; j < solution[i].length; j++) {
+      if (solution[i][j] !== solution[i + 1][j]) {
+        diffCount++;
+      }
+    }
+
+    if (diffCount !== 1) {
       return false;
     }
   }
+
   return true;
 }
 
-function getFrequencyCount(inputArray: string[]) {
-  const charCounts: { [key: string]: number } = {};
-
-  for (const str of inputArray) {
-    for (const char of str) {
-      charCounts[char] = (charCounts[char] || 0) + 1;
+function tryRearranging(
+  inputArray: string[],
+  ret: boolean,
+  currentSolution: any[],
+) {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (ret) break;
+    currentSolution.push(inputArray.splice(i, 1)[0]);
+    if (inputArray.length === 0) {
+      ret = ret || isValidSolution(currentSolution);
+    } else {
+      tryRearranging(inputArray, ret, currentSolution);
     }
+    inputArray.splice(i, 0, currentSolution.pop());
   }
-
-  return charCounts;
 }
-
-console.log(stringRearrangement(["aba", "bbb", "bab"]));
