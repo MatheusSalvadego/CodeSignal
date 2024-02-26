6a768942b9d789947f5d048fa91e5afc0d4c3d0e
Salvadego Matheus <matheus.salvadego@escola.pr.gov.br>
2024-02-26 11:19:36 -0300

    Extra Number

---
+++ Arcade/The Core/At the Crossroads/extraNumber.ts
@@ -0,0 +1,3 @@
+function extraNumber(a: number, b: number, c: number): number {
+  return a == b ? c : a == c ? b : a;
+}
---
+++ Arcade/The Core/At the Crossroads/knapsackLight.ts
@@ -0,0 +1,25 @@
+function knapsnackLight(
+  value1: number,
+  weight1: number,
+  value2: number,
+  weight2: number,
+  maxW: number,
+): number {
+  if (weight1 + weight2 <= maxW) {
+    return value1 + value2;
+  }
+
+  if (weight1 <= maxW && weight2 <= maxW) {
+    return Math.max(value1, value2);
+  }
+
+  if (weight1 <= maxW) {
+    return value1;
+  }
+
+  if (weight2 <= maxW) {
+    return value2;
+  }
+
+  return 0;
+}
